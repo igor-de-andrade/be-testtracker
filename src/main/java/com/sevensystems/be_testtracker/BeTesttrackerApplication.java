6135034f87c76d7cb823b8cb51ed7243bf4b2c84
igor-de-andrade <cadastros.igordeandrade@gmail.com>
2025-07-08 20:52:27 -0300

    feat: adiciona a estrutura base do projeto

---
+++ src/main/java/com/sevensystems/be_testtracker/BeTesttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sevensystems.be_testtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTesttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTesttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevensystems/be_testtracker/BeTesttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevensystems.be_testtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTesttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
